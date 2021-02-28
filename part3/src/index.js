e1364675532af0998824934fafb08aa101e4a94d
Irene Monzon <irene.monzon@lvaindices.com>
2021-02-28 00:35:45 -0300

    react component

---
+++ part3/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById("root")
+
+const App=(props)=>{
+  return(
+    <div>
+      <h1>{props.contadorInicial}</h1>
+    </div>
+  ) 
+}
+
+let contador=0;
+const refresh=()=>{
+  ReactDOM.render(
+    <App contadorInicial={contador} />,
+    rootElement
+  );
+}
+setInterval(()=>{
+  refresh()
+  contador++
+},1000)
+
+
+
